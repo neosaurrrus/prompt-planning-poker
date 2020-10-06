5300bf244a1062e654ccb216cbd85bde98e749ba
neosaurrrus <lukiek@gmail.com>
2020-10-06 15:14:45 +0100

    fix cors issue usi g rack cors gem

--- prompt_planning_poker_frontend/src/components/Plans.js
+++ prompt_planning_poker_frontend/src/components/Plans.js
@@ -4,12 +4,17 @@ import Plan from './Plan'
 
 class Plans extends Component {
 
+  componentDidMount() {
+    console.log(this.props)
+    this.props.getPlans()
+  }
+
   render() {
     return(
       <div>
         <h2>Plans</h2>
         
-        {this.props.plans.map( plan => <Plan deletePlan={this.props.deletePlan} plan={plan}/>)}
+        {/* {this.props.plans.map( plan => <Plan deletePlan={this.props.deletePlan} plan={plan}/>)} */}
 
       </div>
     );
--- prompt_planning_poker_frontend/src/containers/PlansContainer.js
+++ prompt_planning_poker_frontend/src/containers/PlansContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import PlanInput from '../components/PlanInput';
 import Plans from '../components/Plans';
+import { getPlans } from '../actions/planActions'
 class PlansContainer extends Component {
 
   state = {}
@@ -9,24 +10,31 @@ class PlansContainer extends Component {
   newPlan = () =>{
     this.props.addPlan("test")
   }
+
   render() {
     return (
       <div>
         <PlanInput addPlan={this.props.addPlan} />
-        <Plans plans={this.props.plans} deletePlan={this.props.deletePlan} />
+        <Plans getPlans={this.props.getPlans} deletePlan={this.props.deletePlan} />
       </div>
     )
   }
 }
+// WHY is there no plans in the state?
 
-
-const mapStateToProps = state => { return {
-  plans: state.plans  
-}}
+const mapStateToProps = state => { 
+  return {plans: state.plans}
+}
   
-const mapDispatchToProps = dispatch => ({
-  addPlan: plan  => dispatch({ type: "ADD_PLAN", plan}),
-  deletePlan: id => dispatch({ type: "DELETE_PLAN", id})
-})
+// const mapDispatchToProps = dispatch => ({
+//   addPlan: plan  => dispatch({ type: "ADD_PLAN", plan}),
+//   deletePlan: id => dispatch({ type: "DELETE_PLAN", id})
+// })
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getPlans: () => dispatch(getPlans())
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PlansContainer);

572eaab416ab74145d6e74cb50e1d83bb4286bfa
Lukie Kang <lukiek@gmail.com.com>
2020-10-07 23:21:56 +0100

    firt success at getting dynamic routes based on plan url

--- prompt_planning_poker_frontend/src/components/PlanInput.js
+++ prompt_planning_poker_frontend/src/components/PlanInput.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
+import cuid from 'cuid'
+import {Redirect} from 'react-router-dom'
+import Plan from '../components/Plan'
 
 class PlanInput extends Component {
 
   state = {name:"", 
-           owner:""
+           owner:"",
+           complete: false
           }
 
   handleChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.addPlan({name: this.state.name, owner: this.state.owner})
+    const url = cuid()
+    this.props.addPlan({name: this.state.name, owner: this.state.owner, url})
+    this.setState({
+      complete: true,
+      completedUrl: `/plans/${url}`
+    })
+
   }
   render() {
+    if (this.state.complete === true) {
+      return <Redirect to={this.state.completedUrl}>
+        <Plan plan={{plan:{name:this.state.name, owner: this.state.owner}}}></Plan>
+        </Redirect>
+    }
     return (
-      <div>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="name">Give the plan a name</label>
           <input type="text" onChange={this.handleChange} name="name" value={this.state.name}/>
@@ -26,7 +40,7 @@ class PlanInput extends Component {
           <input type="text" onChange={this.handleChange} name="owner" value={this.state.owner}/>
           <input type="submit"></input>
         </form>
-      </div>
+
     );
   }
 };
--- prompt_planning_poker_frontend/src/containers/PlansContainer.js
+++ prompt_planning_poker_frontend/src/containers/PlansContainer.js
@@ -2,27 +2,64 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import PlanInput from '../components/PlanInput';
 import Plans from '../components/Plans';
+import Plan from '../components/Plan';
 
 import { getPlans, addPlan, deletePlan } from '../actions/planActions'
-// import {addPlan} from '../actions/planActions'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+
+} from "react-router-dom";
+
 
 class PlansContainer extends Component {
-
 
   componentDidMount(){
     this.props.getPlans()
   }
 
+
+
+ 
+
   render() {
     return (
-      <div>
-        <PlanInput addPlan={this.props.addPlan} />
-        <Plans getPlans={this.props.getPlans} deletePlan={this.props.deletePlan} plans={this.props.plans}/>
-      </div>
+      // <div>
+      //   <PlanInput addPlan={this.props.addPlan} />
+      //   <Plans getPlans={this.props.getPlans} deletePlan={this.props.deletePlan} plans={this.props.plans}/>
+      // </div>
+        <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/new-plan">New Plan</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/new-plan">
+              <PlanInput addPlan={this.props.addPlan} getPlanUrl={this.getPlanUrl} />
+            </Route>
+            <Route path="/plans/:url">
+            <Plan deletePlan={this.props.deletePlan}  />
+            </Route> 
+          </Switch>
+        </div>
+        </Router>
+
+
     )
   }
 }
 
+PlansContainer.defaultProps= {
+  plans: [{id:"not loaded",
+           url:"not loaded"}]
+}
 
 const mapStateToProps = state => { 
   return {plans: state.plans}

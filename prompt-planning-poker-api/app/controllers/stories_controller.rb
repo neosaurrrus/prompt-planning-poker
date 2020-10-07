2f4d54185363b9b143e2cff44ed236d9f338964d
Lukie Kang <Lukie Kang>
2020-10-07 11:29:14 +0100

    add stories controller plan relationships

--- prompt-planning-poker-api/app/controllers/stories_controller.rb
+++ prompt-planning-poker-api/app/controllers/stories_controller.rb
@@ -1,2 +1,49 @@
 class StoriesController < ApplicationController
+    before_action :get_plan
+    before_action :set_story, only: [:show, :edit, :update, :destroy]
+
+
+    def index
+        stories = plan.stories
+        render json: stories
+    end
+    def show
+        story = Story.find(params[:id])
+        render json: story
+    end
+
+    def create 
+        story = plan.story.build!(story_params)
+        render json: story
+    end
+
+    def update
+        story.update(story_params)
+        render json: story
+    end
+
+    def destroy
+        story.delete
+        render json: Story.all
+    end
+
+    private
+
+    private
+  def get_plan
+     plan = Plan.find(params[:plan_id])
+  end
+
+  def set_story
+    story = Plan.stories.find(params[:id])
+  end
+
+    def story_params
+        params.require(:story).permit(:id, :name, :owner, :url, :complete)
+    end
+
 end
+
+
+
+

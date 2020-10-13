3c31d81c9cac4f5490083ab10b3573c2287ccf6e
Lukie Kang <Lukie Kang>
2020-10-13 22:50:39 +0100

    still working on story deletion bug

--- prompt_planning_poker_frontend/src/components/Story.js
+++ prompt_planning_poker_frontend/src/components/Story.js
@@ -10,22 +10,21 @@ class Story extends Component{
     renderStory = () => { 
     if (this.state.story) return (
     <div>
-        <p>As a <strong>{this.state.story.as_a}</strong>,</p>
-        <p>I want to <strong>{this.state.story.want_to}</strong></p>
-        <p>So I can <strong>{this.state.story.i_can}</strong></p>
+        <p>As a <strong>{this.props.story.as_a}</strong>,</p>
+        <p>I want to <strong>{this.props.story.want_to}</strong></p>
+        <p>So I can <strong>{this.props.story.i_can}</strong></p>
         <button onClick={this.handleDelete}>Delete Story</button>
         <hr></hr>
      </div>
     )
     }
-    handleDelete = () => {
+    handleDelete = (e) => {
+        e.preventDefault()
         this.setState = ({
             story: {}
         })
         this.props.deleteStory(this.props.story.id, this.props.plan) 
-        
-
-      }
+    }
     render(){
         return (
             this.renderStory()

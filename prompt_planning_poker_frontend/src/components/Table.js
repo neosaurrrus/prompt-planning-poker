e08a1aeb631890c892362a166630941442609071
Lukie Kang <lukiek@gmail.com.com>
2020-10-15 12:21:40 +0100

    add placeholder components for PokerContainer

---
+++ prompt_planning_poker_frontend/src/components/Table.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Table extends Component{
+  
+    render(){
+        return (
+            <div>
+                <h2>Table</h2>
+            <p>Will show current player moves</p>
+            </div>
+            
+        )
+    }
+
+}
+
+export default Table
+

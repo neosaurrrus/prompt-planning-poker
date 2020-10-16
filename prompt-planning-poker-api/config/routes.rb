37ec5c525b2fb7c79cdafa6658d5813013bc6d0a
Lukie Kang <Lukie Kang>
2020-10-16 14:17:48 +0100

    add functionality to add/replace a player

--- prompt-planning-poker-api/config/routes.rb
+++ prompt-planning-poker-api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :plans do
-    resources :stories
+    resources :stories do
+      resources :players
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

59d33867c56751a53013f320c7f0150341a4f882
Lukie Kang <lukiek@gmail.com.com>
2020-10-19 07:43:25 +0100

    add reveal functionality

--- prompt_planning_poker_frontend/src/components/Table.js
+++ prompt_planning_poker_frontend/src/components/Table.js
@@ -3,14 +3,30 @@ import Player from './Player'
 
 class Table extends Component{
   
+    state = {
+        reveal: false
+    }
+
+    handleReveal = (e) => {
+        this.setState({
+            reveal: !this.state.reveal
+        })
+    }
+
+    handleReset = (e) => {
+        //deletes all players from a story
+        
+    }
     renderPlayers = () => {
-        if (this.props.story) return this.props.story.players.map(player => <Player player={player} key={player.id}/>)
+        if (this.props.story) return this.props.story.players.map(player => <Player reveal={this.state.reveal} player={player} key={player.id}/>)
     }
     render(){
         return (
             <div>
                 <h2>Table</h2>
                 {this.renderPlayers()}
+                <button onClick={this.handleReveal} name='reveal'>Reveal</button>
+                <button onClick={this.handleReset} name='reset'>Reset</button> 
             </div>
             
         )

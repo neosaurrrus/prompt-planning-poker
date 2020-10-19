f5596d375ffe33afe828c039ea52f08af8b7b933
Lukie Kang <lukiek@gmail.com.com>
2020-10-20 00:07:08 +0100

    add form toggle to stories, minor styling to stories

--- prompt_planning_poker_frontend/src/components/StoryInput.js
+++ prompt_planning_poker_frontend/src/components/StoryInput.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
 class StoryInput extends Component {
-  state = {as_a:"", 
+  state = {
+    story:{as_a:"", 
            want_to:"",
            i_can:"",
            complete: false
           }
 
+    }
+
   handleChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value,
+      story:{
+        [e.target.name]: e.target.value
+      }
+      
     })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()              
-    this.props.addStory(this.state,this.props.plan)
+    this.props.addStory(this.state.story,this.props.plan)
     this.setState({
       complete: true,
       completedUrl: `/`
@@ -28,14 +34,21 @@ class StoryInput extends Component {
     //     </Redirect>
     // }
     return (
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">As a </label>
-          <input type="text" onChange={this.handleChange} name="as_a" value={this.state.as_a}/>
-          <label htmlFor="owner">I would want to</label>
-          <input type="text" onChange={this.handleChange} name="want_to" value={this.state.want_to}/>
-          <label htmlFor="owner">So I can</label>
-          <input type="text" onChange={this.handleChange} name="i_can" value={this.state.i_can}/>
-          <input type="submit"></input>
+        <form className='story-input' style={{ display: this.props.formDisplay }}onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="name">As a </label>
+            <input type="text" onChange={this.handleChange} name="as_a" value={this.state.story.as_a}/> , 
+          </div>
+          <div>
+            <label htmlFor="owner">I would want to</label>
+            <input type="text" onChange={this.handleChange} name="want_to" value={this.state.story.want_to}/> , 
+          </div>
+          <div>
+            <label htmlFor="owner">So I can</label>
+            <input type="text" onChange={this.handleChange} name="i_can" value={this.state.story.i_can}/>.
+          </div>
+         
+          <input type="submit" value='Add Story'></input>
         </form>
 
     );

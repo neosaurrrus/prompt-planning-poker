fea9b0c7e6805b96df694c0c8b30da9c95640f68
neosaurrrus <lukiek@gmail.com>
2020-10-19 07:43:25 +0100

    add reveal functionality

--- prompt_planning_poker_frontend/src/components/Player.js
+++ prompt_planning_poker_frontend/src/components/Player.js
@@ -3,39 +3,14 @@ import React, { Component } from 'react';
 
 class Player extends Component{
      
-    
-
-    // handleDelete = (e) => {
-    //     // e.preventDefault() //doesnt make a difference?
-    //     this.props.deleteStory(this.props.story.id, this.props.plan) 
-    // }
-    // handleSelect= (e) => {
-    //     e.preventDefault()
-    //     this.props.editPlan(this.props.plan, {selectedStory: this.props.story.id}) 
-    // }
-
-    // checkSelected = () =>{
-    //     if (this.props.plan.selectedStory === this.props.story.id ) {
-    //         return <h4>Selected</h4>
-    //     }
-    // }
-    // renderStory = () => { 
-    // if (this.props.story) return (
-    // <div>
-    //     <p>As a <strong>{this.props.story.as_a}</strong>,</p>
-    //     <p>I want to <strong>{this.props.story.want_to}</strong></p>
-    //     <p>So I can <strong>{this.props.story.i_can}</strong></p>
-    //     <button onClick={this.handleSelect}>Select Story</button>
-    //     <button onClick={this.handleDelete}>Delete Story</button>
-    //     <hr></hr>
-    //  </div>
-    // )
-    // }
+    renderReveal = () => {
+        return this.props.reveal ? this.props.player.score : '???'
+    }
 
     render(){
         return (
             <div>
-                {this.props.player.name} - {this.props.player.score}
+                {this.props.player.name} - {this.renderReveal()}
             </div>
            
          )

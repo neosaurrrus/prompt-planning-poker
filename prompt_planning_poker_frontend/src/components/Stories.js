da2530841d44dce1cea0a3af0c57920cebe47f89
Lukie Kang <Lukie Kang>
2020-10-20 13:49:18 +0100

    I dont know what I changed but it probably minor

--- prompt_planning_poker_frontend/src/components/Stories.js
+++ prompt_planning_poker_frontend/src/components/Stories.js
@@ -7,7 +7,22 @@ class Stories extends Component{
   
     render(){
 
-        if (this.props.plan.stories) return this.props.plan.stories.map(story => <Story story={story} key={story.id} plan={this.props.plan} deleteStory={this.props.deleteStory} editPlan={this.props.editPlan}/>)
+        if (this.props.plan.stories){
+            const stories = this.props.plan.stories
+            const selectedStory =  stories.find( story => story.id === this.props.plan.selectedStory)
+            const otherStories =  stories.filter( story => story.id !== this.props.plan.selectedStory)
+            console.log(selectedStory)
+            return (
+                <div>
+                    <h4>Selected Story</h4>
+                     <Story story={selectedStory} key={selectedStory.id} plan={this.props.plan} deleteStory={this.props.deleteStory} editPlan={this.props.editPlan}/>
+                     <h4>Other Stories</h4>
+                     {otherStories.map(story => <Story story={story} key={story.id} plan={this.props.plan} deleteStory={this.props.deleteStory} editPlan={this.props.editPlan}/>)}
+                </div>
+               
+            )
+        } 
+        
         return "Loading Stories"
     }
 

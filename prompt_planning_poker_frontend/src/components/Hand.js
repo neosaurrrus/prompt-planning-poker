25510bea65223dca69a9d2b97f342e393a25a84a
Lukie Kang <lukiek@gmail.com.com>
2020-10-22 12:22:31 +0100

    Add card style animation and submission

--- prompt_planning_poker_frontend/src/components/Hand.js
+++ prompt_planning_poker_frontend/src/components/Hand.js
@@ -1,42 +1,28 @@
 import React, { Component } from 'react';
 
-
-
-
 class Hand extends Component{
-  
-    state = {
-        score: null,
-    }
 
     handleClick = (e) => {
-        this.setState({
-          score: e.target.value,
-        })
-      }
- 
-    
-    handleSubmit = (e) => {
-      this.props.addPlayer(this.props.plan,this.props.story, {name:this.props.user.userName,score: this.state.score})
+      this.props.addPlayer(this.props.plan,this.props.story, {name:this.props.user.userName,score: Number(e.target.innerText)})
   }
+  
 
     render(){
         return (
             <div className='Hand'>
               <h2>Choose a Card to Play</h2>
-              <p>Sexy cards</p>
-          
-              <button onClick={this.handleClick} name='1' value='1'>1</button>
-              <button onClick={this.handleClick} name='2' value='2'>2</button>
-              <button onClick={this.handleClick} name='3' value='3'>3</button>
-              <button onClick={this.handleClick} name='4' value='5'>5</button>
-              <button onClick={this.handleClick} name='5' value='8'>8</button>
-              <button onClick={this.handleClick} name='13' value='13'>13</button>
-              <button onClick={this.handleClick} name='21' value='21'>21</button>
-              <button onClick={this.handleClick} name='34' value='34'>34</button>
-              <button onClick={this.handleClick} name='55' value='55'>55</button>
-              <button onClick={this.handleClick} name='89' value='89'>89</button>
-              <button onClick={this.handleSubmit} name='submit'>Submit Score</button>
+                  <div className='card-container'>
+                  <div className='card card-1' onClick={this.handleClick} name='1' value='1'>1</div>
+                  <div className='card card-2' onClick={this.handleClick} name='2' value='2'>2</div>
+                  <div className='card card-3' onClick={this.handleClick} name='3' value='3'>3</div>
+                  <div className='card card-4' onClick={this.handleClick} name='4' value='5'>5</div>
+                  <div className='card card-5' onClick={this.handleClick} name='5' value='8'>8</div>
+                  <div className='card card-6' onClick={this.handleClick} name='13' value='13'>13</div>
+                  <div className='card card-7' onClick={this.handleClick} name='21' value='21'>21</div>
+                  <div className='card card-8' onClick={this.handleClick} name='34' value='34'>34</div>
+                  <div className='card card-9' onClick={this.handleClick} name='55' value='55'>55</div>
+                  <div className='card card-10' onClick={this.handleClick} name='89' value='89'>89</div>
+                </div>
             </div>
         )
     }

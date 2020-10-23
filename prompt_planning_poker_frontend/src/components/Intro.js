f80cd682abaa64e5abada3960bc9d79fcc810d40
Lukie Kang <lukiek@gmail.com.com>
2020-10-23 16:34:03 +0100

    CSS formatting bug fix with footer and gradient

--- prompt_planning_poker_frontend/src/components/Intro.js
+++ prompt_planning_poker_frontend/src/components/Intro.js
@@ -6,6 +6,7 @@ function Intro(){
     return (
         <article>
             <h1>Wecome to Prompt Planning Poker</h1>
+            <br></br>
             <p>This is placeholder text</p>
             <p>This is placeholder text</p>
             <p>This is placeholder text</p>

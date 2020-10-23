0c5e05b858fdb1149b96d642492716b38f4c005e
Lukie Kang <Lukie Kang>
2020-10-23 16:49:14 +0100

    Add placeholder intro

--- prompt_planning_poker_frontend/src/components/Intro.js
+++ prompt_planning_poker_frontend/src/components/Intro.js
@@ -4,13 +4,15 @@ import React from 'react';
 function Intro(){
  
     return (
-        <article>
-            <h1>Wecome to Prompt Planning Poker</h1>
+        <article id='intro'>
+            <h1>Prompt Planning Poker</h1>
             <br></br>
-            <p>This is placeholder text</p>
-            <p>This is placeholder text</p>
-            <p>This is placeholder text</p>
-            <p>This is placeholder text</p>
+            <p>This is an app that allows you to perform Planning Poker with you team, online, with minimal fuss. </p>
+                <p>How easy is it? Start a new session to find out!</p>
+            <h3>What is Planning Poker?</h3>
+            <p>Planning poker, also called Scrum poker, is a consensus-based, gamified technique for estimating, mostly used to estimate effort or relative size of development goals in software development. In planning poker, members of the group make estimates by playing numbered cards face-down to the table, instead of speaking them aloud. The cards are revealed, and the estimates are then discussed.</p>
+            <h3>Why do it?</h3>
+            <p>The reason to use planning poker is to avoid the influence of the other participants. If a number is spoken, it can sound like a suggestion and influence the other participants' sizing. Planning poker should force people to think independently and propose their numbers simultaneously. This is accomplished by requiring that all participants show their card at the same time.</p>
         </article>
         
 
